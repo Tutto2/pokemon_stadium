ae86892b37ae6b17b01816c78bbc04b018d01a00
tutto2 <tuto.2@hotmail.com>
2023-11-21 15:40:20 -0500

    refactor update

--- action.rb
+++ action.rb
@@ -1,7 +1,7 @@
 require_relative "pokemon/pokemon"
 
 class Action
-  attr_reader :action_type, :priority, :action
+  attr_reader :action_type, :priority, :action, :speed
   
   def initialize(action_type: , priority: 0, action: nil, speed: nil)
     @action_type = action_type
@@ -10,4 +10,42 @@ class Action
     @speed = speed
   end
 
+  def perform_action
+    case action_type
+    when :switch_action
+      perform_switch
+    when :atk_action
+      perform_attack
+    when :additional_action
+      additional_action
+    end
+  end
+
+  private
+  
+  def perform_switch(action_message)
+    next_pokemon = action_message[:next_pokemon]
+    player = action_message[:player]
+      
+    if player == :one
+      @current_pokemon_p1 = next_pokemon
+    elsif player == :two
+      @current_pokemon_p2 = next_pokemon
+    end
+  end
+
+  def perform_attack(action_message)
+    attk_num = action_message[:attk_num]
+    player = action_message[:player]
+      
+    if player == :one
+      target_pokemon = @current_pokemon_p2
+      current_pokemon = @current_pokemon_p1
+    elsif player == :two
+      target_pokemon = @current_pokemon_p1
+      current_pokemon = @current_pokemon_p2
+    end
+
+    current_pokemon.attack!(attk_num, target_pokemon) unless current_pokemon.fainted?
+  end
 end
--- battleground.rb
+++ battleground.rb
@@ -104,7 +104,7 @@ class PokemonBattleField
   def perform_actions(queue)
     queue.priority_table.each do |index, action|
       if action.size == 1
-        perform_action(action)
+        action.perform_action
       elsif action != empty?
         actions = []
         action.each do |action|
@@ -119,152 +119,10 @@ class PokemonBattleField
     end
   end
 
-  def select_action(player, current_pokemon)
-    case player
-    when :one 
-      previous_atk = action_p1.nil? ? 0 : action_p1[:attk_num]
-      pokemons = pokemons_p1
-    when :two
-      previous_atk = action_p2.nil? ? 0 : action_p2[:attk_num]
-      pokemons = pokemons_p2
-    end
-    action_num = action_index(player)
-
-    case action_num
-    when 1
-      attack_act(player, current_pokemon, previous_atk)
-    when 2
-      switch_act(player, current_pokemon, pokemons)
-    end
-  end
-
-  def action_index(player)
-    puts "1- Attack"
-    puts "2- Change pokemon"
-    print "Player #{player}, what do you want to do?: "
-    num = gets.chomp.to_i
-
-    return num if num == 1 || num == 2
-
-    puts "Not a valid option. Try again"
-    return action_index(player)
-  end
-
-  def attack_act(player, current_pokemon, previous_atk)
-    if current_pokemon.is_attacking?
-      player == :one ? action_p1 : action_p2
-    else
-      current_pokemon.view_attacks
-      go_back(current_pokemon.attacks)
-      attk_num = select_attack(current_pokemon, previous_atk)
-
-      if (1..4).include?(attk_num)
-        {
-          action: ATK_ACTION,
-          attk_num: attk_num,
-          player: player
-        }
-      else
-        return select_action(player, current_pokemon)
-      end
-    end
-  end
-
-  def select_attack(current_pokemon, previous_atk)
-    print 'Enter the attack number: '
-    attk_num = gets.chomp.to_i
-
-    if current_pokemon.has_banned_attack? && attk_num == previous_atk
-      puts "This move can't be used twice in a row"
-      return select_attack(current_pokemon, previous_atk)
-    end
-
-    return attk_num if (1..5).include?(attk_num)
-
-    puts "Not a valid option, try again."
-    return select_attack(current_pokemon, previous_atk)
-  end
-
-  def switch_act(player, current_pokemon, pokemons)
-    next_pokemon = switch_pokemon(player, current_pokemon, pokemons)
-        
-    {
-      action: SWITCH_ACTION,
-      player: player,
-      next_pokemon: next_pokemon
-    }
-  end
-
-  def switch_pokemon(player, current_pokemon, pokemons)
-    pokemon_index = pokemons[selection_index(player, pokemons.length + 1)]
-
-    return select_action(player, current_pokemon) if pokemon_index.nil?
-
-    if current_pokemon == pokemon_index
-      puts "That's your current pokemon, pick another one."
-      return switch_pokemon(player, current_pokemon, pokemons)
-    elsif pokemon_index.fainted?
-      puts "#{pokemon_index} is fainted, pick another one."
-      return switch_pokemon(player, current_pokemon, pokemons)
-    end
-
-    current_pokemon.stats.each do |stat|
-      stat.reset_stat
-    end
-    current_pokemon.reinit_metadata
-    pokemon_index
-  end
-
-  def priority_queue
-    lambda do |action_a, action_b|
-      current_pokemon = action_a[:player] == :one ? @current_pokemon_p1 : @current_pokemon_p2
-      target_pokemon = action_b[:player] == :one ? @current_pokemon_p1 : @current_pokemon_p2
-
-      current_pokemon.atk_priority(action_a[:attk_num]) <=> target_pokemon.atk_priority(action_b[:attk_num])
-    end
-  end
-
   def perform
-    ->(action) { perform_action(action) }
+    ->(action) { action.perform_action }
   end
-
-  def perform_action(action)
-    case action.action_type
-    when :switch_action
-      perform_switch(action)
-    when :atk_action
-      perform_attack(action)
-    when :additional_action
-
-    end
-  end
-
-  def perform_switch(action_message)
-    next_pokemon = action_message[:next_pokemon]
-    player = action_message[:player]
-      
-    if player == :one
-      @current_pokemon_p1 = next_pokemon
-    elsif player == :two
-      @current_pokemon_p2 = next_pokemon
-    end
-  end
-
-  def perform_attack(action_message)
-    attk_num = action_message[:attk_num]
-    player = action_message[:player]
-      
-    if player == :one
-      target_pokemon = @current_pokemon_p2
-      current_pokemon = @current_pokemon_p1
-    elsif player == :two
-      target_pokemon = @current_pokemon_p1
-      current_pokemon = @current_pokemon_p2
-    end
-
-    current_pokemon.attack!(attk_num, target_pokemon) unless current_pokemon.fainted?
-  end
-
+  
   def go_back(list)
     puts "#{ (list.length) + 1 }- Go back"
     puts
